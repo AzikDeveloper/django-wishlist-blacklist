e63f234f9265eb6639983d1f1f7f22e544e2b434
azikdev <a.khushnazarov@uic.group>
2022-08-11 13:01:00 +0500

    ref

--- django_wishlist_blacklist/apps.py
+++ django_wishlist_blacklist/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangoWishlistBlacklistConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src.django_wishlist_blacklist'
+    name = 'django_wishlist_blacklist'
