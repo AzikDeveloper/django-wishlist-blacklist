fe9eb5ab16df507a1ad71be2fe4a44cf2f772801
azikdev <a.khushnazarov@uic.group>
2022-08-11 13:00:49 +0500

    ref

--- django_wishlist_blacklist/apps.py
+++ django_wishlist_blacklist/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BinderConfig(AppConfig):
+class DjangoWishlistBlacklistConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'src.django_wishlist_blacklist'
--- django_wishlist_blacklist/signals.py
+++
@@ -1,7 +0,0 @@
-from django.dispatch import Signal
-
-post_add_to_wishlist = Signal()
-post_remove_from_wishlist = Signal()
-
-post_add_to_blacklist = Signal()
-post_remove_from_blacklist = Signal()
